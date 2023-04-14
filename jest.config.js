eec57f580cf1206f4916c813114d452ff14ef887
thommccarthy <mccarttj@gmail.com>
2023-04-14 17:47:05 -0400

    project setup

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+};
